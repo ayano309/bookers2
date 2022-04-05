0e71e26e250973329dfd6f083b04429795a59668
ayano309 <ayatomato.k@gmail.com>
2022-04-05 02:23:39 +0000

    userとbookの紐付け

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,52 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @books = Book.all
+    @book = current_user.books.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    
   end
 
-  def edit
+  def create
+    @book = current_user.books.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created'
+    else
+      @books = Book.all
+      render :index
+    end
   end
+
+
+  def edit
+     @book=current_user.books.find(params[:id])
+
+  end
+  def update
+    @book=current_user.books.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book=current_user.books.find(params[:id])
+    book.destroy!
+    redirect_to books_path,notice: "Book was successfully destroyed."
+
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  
-  
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
   devise_for :users
   root to: 'homes#index'
   get 'homes/about'
+  resources :books
 end
