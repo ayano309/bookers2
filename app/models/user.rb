4744e927ac704ae6259b7997c1439240c3c6137b
ayano309 <ayatomato.k@gmail.com>
2022-04-06 09:18:56 +0000

    [update] error表示できるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
   
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+  
   def has_posted?(book)
     books.exists?(id: book.id)
   end
